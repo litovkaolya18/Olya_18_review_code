c3da0e96c988f0c653ce2dcdabe45929005944f9
litovkaolya18 <olakim710@gmail.com>
2026-04-20 19:18:23 +0500

    Add files via upload

---
+++ laba1/laba1.2/Task04.cpp
@@ -0,0 +1,60 @@
+﻿//Во всех заданиях данной подгруппы предполагается, что исходные строки, определяющие выражения, 
+//не содержат пробелов.При выполнении заданий не следует использовать оператор цикла.
+//Вывести значение целочисленного выражения, заданного в виде строки S.Выражение
+//определяется следующим образом(функция M возвращает максимальный из своих параметров,
+//а функция m — минимальный) :
+//        <выражение> :: = <цифра> | M(<выражение>, <выражение>) | m(<выражение>, <выражение>)
+
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+
+int element(string str) {
+    if (str[0] == 'M' || str[0] == 'm') {
+        return calc(str.substr(2, str.size() - 3)); // Убираем "M(" и ")"
+    }
+    return stoi(str);
+}
+
+int calc(string str) {
+    int i = 0;
+    int level = 0;
+    int pos = -1;
+
+    while (i < str.size()) {
+        if (str[i] == '(') level++;
+        if (str[i] == ')') level--;
+        if (level == 1 && str[i] == ',') {
+            pos = i;
+            break;
+        }
+        i++;
+    }
+
+    if (pos == -1) {
+        return element(str);
+    }
+
+    string left = str.substr(0, pos);
+    string right = str.substr(pos + 1);
+
+    if (str[0] == 'M') {
+        return max(calc(left), calc(right));
+    }
+    else if (str[0] == 'm') {
+        return min(calc(left), calc(right));
+    }
+
+    return 0; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string str;
+    cout << "Введите выражение: ";
+    getline(cin, str);
+    int r = calc(str);
+    cout << "Ответ: " << r << endl;
+    return 0;
+}
